730fdc3a623671937a7a61f4d2caeea424e6243d
pluiedev <hi@pluie.me>
2022-12-02 14:51:24 +0800

    2022 day 2, minimized

---
+++ src/main/kotlin/me/pluie/aoc/c2022/Day2.kt
@@ -0,0 +1,16 @@
+package me.pluie.aoc.c2022
+import me.pluie.aoc.*
+
+fun main() = challenge(2022, 2) {
+    // good luck figuring out how this ultra-minimized,
+    // totally-after-the-fact solution works!
+    //
+    // i used a big when case for my first attempt and
+    // IDEA unfortunately got rid of it cos its undo history was for some reason
+    // really limited.
+    //
+    // oh well! so it goes.
+
+    submit{lines().sumOf{it[2]-'W'+((it[2]-it[0]).mod(3)-1).mod(3)*3}}
+    submit{lines().sumOf{3*it[2].code-263+(it[0]-'A'+(it[2]-'V').mod(3)).mod(3)}}
+}

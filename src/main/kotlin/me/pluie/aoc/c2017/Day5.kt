ff487d66c173e2cc811dfe70b8a3c73c67871088
pluiedev <hi@pluie.me>
2022-12-05 12:50:16 +0800

    2017 day 5

--- src/main/kotlin/me/pluie/aoc/c2017/Day5.kt
+++ src/main/kotlin/me/pluie/aoc/c2017/Day5.kt
@@ -4,14 +4,27 @@ import me.pluie.aoc.*
 import kotlin.math.*
 
 fun main() = challenge(2017, 5) {
-    val input = l(::int).toMutableList()
+    val input = l(::int)
 
     submit {
+        val i = input.toMutableList()
         var tick = 0
         var pos = 0
-        while (pos < input.size) {
-            val v = input[pos]
-            input[pos]++
+        while (pos < i.size) {
+            val v = i[pos]
+            i[pos]++
+            pos += v
+            tick++
+        }
+        tick
+    }
+    submit {
+        val i = input.toMutableList()
+        var tick = 0
+        var pos = 0
+        while (pos < i.size) {
+            val v = i[pos]
+            i[pos] += if (v >= 3) -1 else 1
             pos += v
             tick++
         }

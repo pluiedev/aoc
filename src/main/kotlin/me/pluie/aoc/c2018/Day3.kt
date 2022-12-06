9df07c179bb7702a4a6dc1d6ae954e49471bf9e8
pluiedev <hi@pluie.me>
2022-12-06 14:29:40 +0800

    2018 day 3

---
+++ src/main/kotlin/me/pluie/aoc/c2018/Day3.kt
@@ -0,0 +1,33 @@
+package me.pluie.aoc.c2018
+
+import me.pluie.aoc.*
+
+fun main() = challenge(2018, 3) {
+    data class Claim(val n: Int, val x: Int, val y: Int, val w: Int, val h: Int)
+    val grid = ByteArray(1000 * 1000) { 0 }
+
+    val claims = l {
+        val (n, x, y, w, h) = it.match("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)")!!.toList().ints()
+        Claim(n, x, y, w, h)
+    }
+
+    claims.forEach {
+        for (y in it.y until it.y + it.h) {
+            for (x in it.x until it.x + it.w) {
+                grid[y * 1000 + x]++
+            }
+        }
+    }
+
+    submit { grid.count { it > 1 } }
+    submit {
+        claims.findLast {
+            for (y in it.y until it.y + it.h) {
+                for (x in it.x until it.x + it.w) {
+                    if (grid[y * 1000 + x] > 1) return@findLast false
+                }
+            }
+            true
+        }!!.n
+    }
+}

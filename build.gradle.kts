d0c45de426aadd31d0f89eca250372c58923aa41
pluiedev <hi@pluie.me>
2022-12-06 09:24:56 +0800

    gradleify

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+group = "me.pluie"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.ginsberg:cirkle:1.0.1")
+}

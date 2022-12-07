0f829c61aea6e5ce1ebb6dd0e7e3a3bf53dc7eb1
pluiedev <hi@pluie.me>
2022-12-07 14:33:57 +0800

    2022 day 7

---
+++ src/main/kotlin/me/pluie/aoc/c2022/Day7.kt
@@ -0,0 +1,63 @@
+package me.pluie.aoc.c2022
+import me.pluie.aoc.*
+
+sealed class Node {
+    abstract val name: String
+    abstract val fs: Map<String, MutableList<Node>>
+
+    val totalSize: Int by lazy { calculateSize() }
+
+    protected abstract fun calculateSize(): Int
+
+    data class File(
+        override val name: String,
+        val size: Int,
+        override val fs: Map<String, MutableList<Node>>
+    ): Node() {
+        override fun calculateSize() = size
+    }
+    data class Directory(
+        override val name: String,
+        override val fs: Map<String, MutableList<Node>>
+    ): Node() {
+        override fun calculateSize() = fs[name.dbg()]!!.sumOf { it.totalSize }
+    }
+}
+
+fun main() = challenge(2022, 7) {
+    val fs = mutableMapOf<String, MutableList<Node>>()
+    val cwd = mutableListOf<String>()
+
+    l().forEach {
+        val cmdMatch = it.match("\\$ (?:(cd) (/|[a-z]+|\\.\\.)|ls)")
+        if (cmdMatch != null) {
+            val (cd, path) = cmdMatch
+
+            if (cd != "") when(path) {
+                ".." -> cwd.removeLast()
+                "/" -> cwd.clear()
+                else -> cwd.add(path)
+            }
+        } else {
+            val (typeOrSize, name) = it.split(" ", limit = 2)
+            val path = cwd.joinToString("/", prefix = "/")
+            val list = fs.getOrPut(path) { mutableListOf() }
+
+            list.add(if (typeOrSize == "dir") {
+                fs[name] = mutableListOf()
+
+                Node.Directory(cwd.joinToString("/", postfix = "/$name"), fs)
+            } else {
+                Node.File(name, typeOrSize.int(), fs)
+            })
+        }
+    }
+
+    val sizes = fs.values.m { v -> v.sumOf { it.totalSize } }
+
+    submit { sizes.filter { it <= 100000L }.sum() }
+
+    val unusedSpace = 70000000 - fs["/"]!!.sumOf { it.totalSize }
+    val requiredSpace = 30000000 - unusedSpace
+    submit { sizes.filter { it >= requiredSpace }.min() }
+}
